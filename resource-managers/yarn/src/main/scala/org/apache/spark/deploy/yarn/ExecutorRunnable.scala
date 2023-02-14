e80593b537f9d0a9340169bae25c5a14fb90b4de
visuOwO <held18@outlook.com>
2023-02-13 23:35:13 -0500

    add a classpath

--- resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/ExecutorRunnable.scala
+++ resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/ExecutorRunnable.scala
@@ -197,6 +197,7 @@ private[yarn] class ExecutorRunnable(
 
     // For log4j configuration to reference
     javaOpts += ("-Dspark.yarn.app.container.log.dir=" + ApplicationConstants.LOG_DIR_EXPANSION_VAR)
+    javaOpts += ("-cp $CLASSPATH")
     val mpirun_options_filename = "/work2/09103/he2295/frontera/debug/spark/mpirun_options"
     val mpirun_options = Source.fromFile(mpirun_options_filename).getLines
 
